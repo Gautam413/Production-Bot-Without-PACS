0baa8b4cbbfb7c21a770feaff8d27fa10f1e9f6c
rjangid631 <rohanjangid631@gmail.com>
2025-04-29 12:57:59 +0530

    for the task management

--- Management/presentation/UI.py
+++ Management/presentation/UI.py
@@ -58,7 +58,7 @@ def user_login(request):
         
     else:
         form = LoginForm()  
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'Login.html', {'form': form})
 
 def Home(request):
     
